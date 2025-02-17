137f96ed2f9e3948c53aebd32afb10544d7713ce
Max London <grahamluk66@gmail.com>
2025-02-17 15:46:16 +0000

    [3] Integrated tRPC router with express

--- src/trpc.ts
+++ src/trpc.ts
@@ -1,11 +1,18 @@
 import { initTRPC } from "@trpc/server";
+import * as trpcExpress from "@trpc/server/adapters/express";
 import {
   createExample as createExampleFunc,
   getExamples as getExamplesFunc,
 } from "./components/example";
 import { CreateExampleInput } from "./types/example.type";
 
-const t = initTRPC.create();
+export const createContext = ({
+  req,
+  res,
+}: trpcExpress.CreateExpressContextOptions) => ({});
+type Context = Awaited<ReturnType<typeof createContext>>;
+
+export const t = initTRPC.context<Context>().create();
 
 export const appRouter = t.router({
   getExamples: t.procedure.query(getExamplesFunc),
